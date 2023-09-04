11e4c1a1d9effafc2612a1579d69ad20055b6109
har2525life <har2525life@outlook.com>
2023-09-04 10:29:14 +0900

    add: create environment react typescript maplibre

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import MapPage from "./page/MapPage";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MapPage />
+  </React.StrictMode>
+);
